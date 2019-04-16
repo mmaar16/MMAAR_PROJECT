43b9d7ae67fc5c0c841ec7da4390553377caa467
AisteKiseliovaite <45029204+Silveruki@users.noreply.github.com>
2019-04-16 18:03:32 +0300

    Moved functionality to read_file()
    Removed the class;
    Implemented Yield to return items from .CSV file into a generator;
    Function's only parameter is the .CSV file name;

--- Aiste_Kiseliovaite/PythonCSVRead.py
+++ Aiste_Kiseliovaite/PythonCSVRead.py
@@ -1,42 +1,19 @@
 import csv
 
-class ClusterInfo:
-    def __init__(self, column1, objNo, upperLeftX, upperLeftY, lowerRightX, lowerRightY, centroidX, centroidY, 
-                 area, convexArea, eccentricity, extent, filledArea, majorAxisLength, perimeter, clusterNo):
-        self.column1 = column1
-        self.objNo = objNo
-        self.upperLeftX = upperLeftX
-        self.upperLeftY = upperLeftY
-        self.lowerRightX = lowerRightX
-        self.lowerRightY = lowerRightY
-        self.centroidX = centroidX
-        self.centroidY = centroidY
-        self.area = area
-        self.convexArea = convexArea
-        self.eccentricity = eccentricity
-        self.extent = extent
-        self.filledArea = filledArea
-        self.majorAxisLength = majorAxisLength
-        self.perimeter = perimeter
-        self.clusterNo = clusterNo
+def read_file(fileName):
+    clusterPointArray = []
+    with open(fileName) as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        line_count = 0
+        for row in csv_reader:
+            if line_count == 0:
+                line_count += 1
+            else:
+                yield(row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7], 
+                                                 row[8], row[9], row[10], row[11], row[12], row[13], row[14], row[15])
+                line_count += 1
 
-clusterPointArray = []
-
-with open('tile2.csv') as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=',')
-    line_count = 0
-    for row in csv_reader:
-        if line_count == 0:
-            line_count += 1
-        else:
-            clusterPointArray.append(ClusterInfo(row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7], 
-                                                 row[8], row[9], row[10], row[11], row[12], row[13], row[14], row[15]))
-            line_count += 1
-
-
-for clusterPoints in clusterPointArray:
-    #print(clusterPoints.objNo + " " + clusterPoints.clusterNo)
-    print(clusterPoints.objNo + " " + clusterPoints.upperLeftX + " " + clusterPoints.upperLeftY + " " + clusterPoints.lowerRightX + " " + 
-          clusterPoints.lowerRightY + " " + clusterPoints.centroidX + " " + clusterPoints.centroidY + " " + clusterPoints.area + " " + clusterPoints.convexArea + " " + 
-          clusterPoints.eccentricity + " " + clusterPoints.extent + " " + clusterPoints.filledArea + " " + clusterPoints.majorAxisLength + " " + clusterPoints.perimeter + " " + 
-          clusterPoints.clusterNo)
+clusterPointArray = read_file('tile2.csv')
+print(clusterPointArray)
+print(next(clusterPointArray))
+print(next(clusterPointArray))
