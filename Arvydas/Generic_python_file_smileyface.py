7ff729bce6d3bb35d0edc0b0113e26bd06d5449e
TheZarathustraDude <47151347+TheZarathustraDude@users.noreply.github.com>
2019-04-19 18:02:34 +0300

    put everything into html_generate function

--- Arvydas/Generic_python_file_smileyface.py
+++ Arvydas/Generic_python_file_smileyface.py
@@ -1,24 +1,35 @@
 import csv
 
-lineno = 0
 
-with open('tile2.csv', 'r') as csvfile:
-    with open('tile2.html', 'w') as htmlfile:
-        csv_reader = csv.reader(csvfile)
+# generating html file **maybe need a custom width and height input
+def html_generate(picture_name, csv_file_name, html_file_name):
+    with open(csv_file_name, 'r') as csvfile:
+        with open(html_file_name, 'w') as htmlfile:
+            csv_reader = csv.reader(csvfile)
 
-        htmlfile.write("<!DOCTYPE html>\n")
-        htmlfile.write("<html>\n")
-        htmlfile.write("<body>\n")
-        htmlfile.write('<img src="tile2_clusters.png" width="1000" height="1000" usemap="#planetmap">\n')
-        htmlfile.write('<map name="planetmap">\n')
+            htmlfile.write("<!DOCTYPE html>\n")
+            htmlfile.write("<html>\n")
+            htmlfile.write("<body>\n")
+            htmlfile.write('<img src=' + picture_name + 'width="1000" height="1000" usemap="#clustermap">\n')
+            htmlfile.write('<map name="clustermap">\n')
 
-        for line in csv_reader:
-            if lineno == 0:
-                lineno += 1
-            else:
-                htmlfile.write('    <area shape="rect" coords="' + line[2] + "," + line[3] + "," + line[4] + "," + line[5]
-                           + '" href="#">\n')
+            lineno = 0
 
-        htmlfile.write("</map>\n")
-        htmlfile.write("</body>\n")
-        htmlfile.write("</html>\n")
+            for line in csv_reader:
+                if lineno == 0:
+                    lineno += 1
+                else:
+                    htmlfile.write('    <area shape="rect" coords="' + line[2] + "," + line[3] + "," + line[4] + "," + line[5]
+                               + '" href="#">\n')
+
+            htmlfile.write("</map>\n")
+            htmlfile.write("</body>\n")
+            htmlfile.write("</html>\n")
+
+
+# function call
+picture_name = "tile2.png"
+csv_file_name = "tile2.csv"
+html_name = "tile2.html"
+
+html_generate(picture_name, csv_file_name, html_name)
