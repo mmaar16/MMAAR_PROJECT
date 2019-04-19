3d9b573aeb5b4607281dcae130675e7a25b143ce
AisteKiseliovaite <45029204+Silveruki@users.noreply.github.com>
2019-04-19 18:17:06 +0300

    Added function documentation
    Tested with whole file;
    Removed 'print' lines;

--- Aiste_Kiseliovaite/PythonCSVRead.py
+++ Aiste_Kiseliovaite/PythonCSVRead.py
@@ -1,5 +1,6 @@
 import csv
 
+## Function read_file() reads .CSV files and returns the contents in a generator with yield
 def read_file(fileName):
     clusterPointArray = []
     with open(fileName) as csv_file:
@@ -11,9 +12,5 @@ def read_file(fileName):
             else:
                 yield(row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7], 
                                                  row[8], row[9], row[10], row[11], row[12], row[13], row[14], row[15])
-                line_count += 1
 
-clusterPointArray = read_file('tile2.csv')
-print(clusterPointArray)
-print(next(clusterPointArray))
-print(next(clusterPointArray))
+clusterPointArray = read_file('5_klasterizuoti_rezultatai.csv') #read_file('tile2.csv')
