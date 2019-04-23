395ff38e1657fa6456c99cd728922f16823806ad
AisteKiseliovaite <45029204+Silveruki@users.noreply.github.com>
2019-04-23 20:17:32 +0300

    Added Doxygen documentation

--- src/index.py
+++ src/index.py
@@ -6,6 +6,7 @@ import cgi, cgitb
 cgitb.enable()
 print "Content-Type: text/plain\r\n\r\n"
 print
+# Global variables.
 form = cgi.FieldStorage()
 COLOR_1 = (120, 120, 255, 255)
 COLOR_2 = (255, 255, 0, 255)
@@ -16,10 +17,8 @@ IMAGE_NAME = "tile2_overlayed.png"
 HTML_FILE_NAME = "index.html"
 NEW_CSV_FILE = "new_file.csv"
 
-#################################################
-# Aiste
-
-## Function read_file() reads .CSV files and returns the contents in a generator with yield
+## Function read_file() reads .CSV files and returns the contents in a generator with yield.
+#  @param fileName The file we read data from.
 def read_file(fileName):
     clusterPointArray = []
     with open(fileName) as csv_file:
@@ -32,13 +31,9 @@ def read_file(fileName):
                 yield(row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7], 
                                                  row[8], row[9], row[10], row[11], row[12], row[13], row[14], row[15])
 
-#################################################
-
-
-#################################################
-# Arvydas
-
-# generating html file **maybe need a custom width and height input
+## Function html_generate() generates an .HTML file containing a list of interactable rectangle objects.
+#  @param picture_name The picture we are applying interactable objects on.
+#  @param csv_file_name The newly generated .CSV file.
 def html_generate(picture_name, csv_file_name):
     with open(csv_file_name, 'r') as csvfile:
         with open(HTML_FILE_NAME, 'w') as htmlfile:
@@ -64,15 +59,10 @@ def html_generate(picture_name, csv_file_name):
             htmlfile.write("</html>\n")
             htmlfile.write(open("js.html", "r").read())
 
-
-#################################################
-
-
-
-#################################################
-# Martynas
-
-# function to update the CSV file (find by objectNo and update clusterNo)
+## Function updateCSVFile() updates the .CSV file.
+#  @param CSVFileName The file we read data from.
+#  @param objectNo The number of the selected object we change the data of.
+#  @param clusterNo The new cluster number of the selected object.
 def updateCSVFile(CSVFileName, objectNo, clusterNo):
     with open(CSVFileName) as csv_file_in, open(NEW_CSV_FILE, 'w') as csv_file_out:
         csv_reader = csv.reader(csv_file_in, quotechar = "'")
@@ -103,13 +93,10 @@ def updateCSVFile(CSVFileName, objectNo, clusterNo):
                 csv_writer.writerow(row)
         csv_writer.writerows(csv_reader)
 
-
-
-#################################################
-
-#################################################
-# Max
-
+## Function changeRGBA() colors the cluster in the image with one of the two colors, dependant on the clusterNo.
+#  @param clusterPointArray Generator of the data that was read from the .CSV file.
+#  @param image The image we draw the clusters on.
+#  @param pixelMap The image where the clusters are marked.
 def changeRGBA(clusterPointArray, image, pixelMap):
     for i in clusterPointArray:
         for j in range(int(i[2]), int(i[4])):
@@ -121,23 +108,30 @@ def changeRGBA(clusterPointArray, image, pixelMap):
                 else:
                     pixelMap[j, k] = COLOR_2
 
-
+## Function changeAlpha() updates the value of the variable alpha.
+#  @param newAlpha The new alpha value.
 def changeAlpha(newAlpha):
     alpha = newAlpha
     return alpha
 
-
+## Function overlay() converts the images into RGBA and blends them together.
+#  @param image The image we draw clusters on.
+#  @param clusters The image which contains the clusters.
+#  @param alpha The alpha channel value.
 def overlay(image, clusters, alpha):
     imageRGBA = image.convert("RGBA")
     clustersRGBA = clusters.convert("RGBA")
     return Image.blend(imageRGBA, clustersRGBA, alpha)
 
-
+## Function saveImage() saves the overlayed images as a new image.
+#  @param image The image we draw clusters on.
+#  @param clusters The image which contains the clusters.
+#  @param alpha The alpha channel value.
 def saveImage(image, clusters, alpha):
     imageBlended = overlay(image, clusters, alpha)
     imageBlended.save(IMAGE_NAME)
 
-
+## Function overlayImage() opens .CSV and image files, calls functions to overlay them together and save the new image.
 def overlayImage():
     clusterPointArray = read_file(NEW_CSV_FILE)
     image = Image.open(MAIN_IMAGE)
@@ -147,12 +141,10 @@ def overlayImage():
     changeRGBA(clusterPointArray, image, pixelMap)
     alpha = changeAlpha(.5)
     saveImage(image, clusters, alpha)
-
-
-#################################################
-
-
-
+    
+## Function generateNewImage() calls functions updateCSVFile(), overlayImage(), html_generate().
+#  @param objectNo The number of the object that will be updated.
+#  @param clusterNo The new cluster number of the object that is being updated.
 def generateNewImage(objectNo, clusterNo):
     updateCSVFile(CSV_FILE,objectNo,clusterNo)
     overlayImage()
